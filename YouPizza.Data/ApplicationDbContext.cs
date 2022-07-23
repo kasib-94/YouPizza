feaa18ad96051a23fae009a6274e08d82130c640
kasib-94 <jakczarski@gmail.com>
2022-07-23 16:42:54 +0200

    before adding identity

---
+++ YouPizza.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YouPizza.Model;
+
+namespace YouPizza.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Sauce>? Sauces { get; set; }
+
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
---
+++ YouPizza.Model/Sauce.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YouPizza.Model;
+
+public class Sauce
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+}
