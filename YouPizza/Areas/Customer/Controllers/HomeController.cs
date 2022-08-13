37f18c899ef97ffe8df1a5936bd0719bc1e960ef
kasib-94 <jakczarski@gmail.com>
2022-08-13 16:24:00 +0200

    creating about us page

---
+++ YouPizza/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YouPizza.Areas.Customer.Controllers;
+[Area("Customer")]
+[Controller]
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+
+    public IActionResult Menu()
+    {
+        return View();
+    }
+}
